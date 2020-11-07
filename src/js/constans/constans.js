11a8a70755fd46b4d6f02b647a9e497824287192
Zavizhenetc <zavizhenetc@gmail.com>
2020-11-07 15:00:07 +0300

    bag fix fetch url

--- src/js/constans/constans.js
+++ src/js/constans/constans.js
@@ -3,5 +3,5 @@ export { TEMPLATE, CARDLIST, SHOW_MORE_BUTTON, URL_API };
 const TEMPLATE = document.querySelector('.template').content;
 const CARDLIST = document.querySelector('.container');
 const SHOW_MORE_BUTTON = document.querySelector('#show-more-button');
-const URL_API = NODE_ENV === 'production' ? require('./flights.json') : 'src/js/constans/flights.json';
+const URL_API = NODE_ENV === 'production' ? require('./flights.json') : '/src/js/constans/flights.json';
 
