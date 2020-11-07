e9f26ad1f32c68a870a09fff594bd347bdd2c7e9
Zavizhenetc <zavizhenetc@gmail.com>
2020-11-07 12:08:57 +0300

    add  showMore

--- src/index.js
+++ src/index.js
@@ -10,11 +10,23 @@ import Card from './js/components/flyCard';
 import CardList from './js/components/flyCardList';
 
 
+
+const TEMPLATE = document.querySelector('.template').content
+const CARDLIST = document.querySelector('.container')
+const SHOW_MORE_BUTTON = document.querySelector('#show-more-button');
+
+const flyCardsList = new CardList(CARDLIST)
 const api = new Api();
 const storage = new DataStorage();
-const TEMPLATE = document.querySelector('.template').content
-const cardList = document.querySelector('.container')
-const flyCardsList = new CardList(cardList)
+// csl(SHOW_MORE_BUTTON);
+getStorageCards();
+
+
+// добавляем еще билетов
+SHOW_MORE_BUTTON.addEventListener('click', () => {
+  showMore(count());
+})
+
 
 // записываем в сторедж данные с сервера 
 api
@@ -25,27 +37,44 @@ api
     .catch((err) => {
       console.log(`Ошибка ${err} `);
     });
-
-
-function getStorageCards(){
-    const storageCards = storage.getCards();
-
-    const filterCards = storageCards.result.flights.map(({ flight }) => flight);
     
+
+// рендер данных
+function getStorageCards(){
+  window.count = 0;
+    const storageCards = storage.getCards();
+    const filterCards = storageCards.result.flights.map(({ flight }) => flight);
+
     csl(filterCards);
 
     const flyCard = filterCards.map((data) => {
       return new Card(data, TEMPLATE).createCard();
     });
-    const cardsSlice = flyCard.slice(0, 30);
+    const cardsSlice = flyCard.slice(0, 4);
     flyCardsList.render(cardsSlice);
- 
   }
 
+  // считаем количество билетов 
+  function count() {
+    let count = window.count;
+    if (isNaN(count) || count === 0) {
+      return 6;
+    }
+    return count + 4;
+  }
+// добавляем еще билетов
+  function showMore(count) {
+    const storageCards = storage.getCards();
+    const filterCards = storageCards.result.flights.map(({ flight }) => flight);
 
-  
-    getStorageCards();
-    // csl (a);
-    // csl(fa);
-    // csl(b.result.flights[0].flight.legs[0].duration);
+    // csl(filterCards);
+
+    const flyCard = filterCards.map((data) => {
+      return new Card(data, TEMPLATE).createCard();
+    });
+    const cardsSlice = flyCard.slice(0, count);
+    flyCardsList.remove();
+    flyCardsList.render(cardsSlice);
+    window.count = cardsSlice.length;
+  }
 
